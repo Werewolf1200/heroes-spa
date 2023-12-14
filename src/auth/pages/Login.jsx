6b7a898278db85dfc5f8f3b29d34ab594b072fbc
Saúl Camacho <electrohead1200@gmail.com>
2023-12-13 20:36:53 -0600

    Login, mantener la sesión activa y Logout del usuario

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,10 +1,17 @@
 import { useNavigate } from "react-router-dom"
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
 
 export const Login = () => {
+
+  const { login } = useContext(AuthContext);
 
   const navigate = useNavigate();
 
   const onLogin = () => {
+
+    login('Carlo');
+
     navigate('/', {
       replace: true
     })
